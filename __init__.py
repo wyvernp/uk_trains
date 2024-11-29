4bbee74e3633ab64888e4bb24d50d4f54544f3fd
wyvernp <35661956+wyvernp@users.noreply.github.com>
2024-11-29 14:54:25 +0000

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,16 @@
+from homeassistant.core import HomeAssistant
+
+DOMAIN = "uk_trains"
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry):
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry):
+    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    return True
