19eb2e00be8adafcba3a2ac44d2a37aa8b07f152
wyvernp <35661956+wyvernp@users.noreply.github.com>
2024-11-29 15:43:02 +0000

    Update __init__.py

--- custom_components/uk_trains/__init__.py
+++ custom_components/uk_trains/__init__.py
@@ -1,16 +1,21 @@
-from homeassistant.core import HomeAssistant
+"""UK Trains integration for Home Assistant."""
 
-DOMAIN = "uk_trains"
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from .const import DOMAIN
 
 async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the UK Trains component."""
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up UK Trains from a config entry."""
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, "sensor")
     )
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry):
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
     await hass.config_entries.async_forward_entry_unload(entry, "sensor")
     return True
